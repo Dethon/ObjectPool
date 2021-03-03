73c7de7d72b0d3f0896aecb47f5e31fcbee14079
Francisco Crespo <jf_crespo@outlook.es>
2021-03-03 09:51:27 +0100

    Changed back PoolReturner to inner class

--- code/ObjectPool.hpp
+++ code/ObjectPool.hpp
@@ -5,41 +5,33 @@
 #include <concepts>
 
 namespace obpool {
-	template<class T> class ObjectPool;
-
 	namespace concepts {
 		template<class T, class... Args>
 		concept Resetable = requires(T obj, Args... args) {
 			{obj.reset(std::declval<Args...>())};
 		};
-	}
-
-	template<class T>
-	class PoolReturner {
-	private:
-		std::weak_ptr<ObjectPool<T>*> m_pool;
-	public:
-		PoolReturner(std::weak_ptr<ObjectPool<T>*> pool) : m_pool{ pool } {}
-		void operator()(T* ptr) {
-			if (auto pool_ptr = m_pool.lock(); pool_ptr) {
-				(*pool_ptr)->release(ptr);
-			}
-		}
-	};
-
-	template<class T>
-	using unique_ptr_pool = std::unique_ptr<T, PoolReturner<T>>;
+	}	
 
 	template<class T>
 	class ObjectPool {
 	private:
-		friend class PoolReturner<T>;
-		PoolReturner<T> m_returner;
+		class PoolReturner {
+			std::weak_ptr<ObjectPool<T>*> m_pool;
+		public:
+			PoolReturner(std::weak_ptr<ObjectPool<T>*> pool) : m_pool{ pool } {}
+			void operator()(T* ptr) {
+				if (auto pool_ptr = m_pool.lock(); pool_ptr) {
+					(*pool_ptr)->release(ptr);
+				}
+			}
+		};
+		PoolReturner m_returner;
 		std::shared_ptr<ObjectPool<T>*> m_aliveFlagPtr;
 		std::vector<T> m_pool;
-		std::vector<unique_ptr_pool<T>> m_reusables;
+		std::vector<std::unique_ptr<T, PoolReturner>> m_reusables;
 
 	public:
+		using ptr = std::unique_ptr<T, PoolReturner>;
 		ObjectPool(const ObjectPool<T>&) = delete;
 		ObjectPool(ObjectPool<T>&&) = delete;
 		ObjectPool<T>& operator=(const ObjectPool<T>&) = delete;
@@ -48,7 +40,7 @@ namespace obpool {
 		template<class... Args> requires std::constructible_from<T, Args...>
 		ObjectPool(size_t size, Args&& ... args) : m_returner{ {} } {
 			m_aliveFlagPtr = std::make_shared<ObjectPool<T>*>(this);
-			m_returner = PoolReturner<T>{ m_aliveFlagPtr };
+			m_returner = PoolReturner{ m_aliveFlagPtr };
 			resize(size, std::forward<Args>(args)...);
 		}
 		virtual ~ObjectPool() {
@@ -56,7 +48,7 @@ namespace obpool {
 		}
 
 		template<class... Args> requires concepts::Resetable<T, Args...>
-		unique_ptr_pool<T> acquire(Args&& ... args) {
+		ptr acquire(Args&& ... args) {
 			if (auto reusable = acquire(); reusable != nullptr) {
 				reusable->reset(std::forward<Args>(args)...);
 				return reusable;
@@ -67,7 +59,7 @@ namespace obpool {
 		}
 
 		template<class... Args>
-		unique_ptr_pool<T> acquire(Args&& ... args) {
+		ptr acquire(Args&& ... args) {
 			if (m_reusables.empty()) {
 				return { nullptr, m_returner };
 			}
@@ -111,4 +103,7 @@ namespace obpool {
 			m_reusables.emplace_back(reusable, m_returner);
 		}
 	};
+
+	template<class T>
+	using unique_ptr_pool = typename ObjectPool<T>::ptr;
 }
